812a27f959a05f59eb26a0f523bb1fc989b921db
Vi Tran <contact.vitran@gmail.com>
2022-01-24 21:11:24 +1100

    GET deliverable complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+useEffect(() => {
+    fetch(
+      "http://localhost:4000/questions"
+    )
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+        setQuestions(data);
+      });
+  }, []);
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList questions={questions} />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function QuestionList() {
+function QuestionList({ questions }) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{
+        questions.map((question) => {
+          return <li key={question.id}>{question.prompt}</li>
+        })
+        }</ul>
     </section>
   );
 }
